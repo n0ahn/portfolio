d7085d8b146c5237d579d1f054a342ed3712056f
noahniemeijer <noahniemeijer0603@gmail.com>
2025-07-13 15:43:50 +0200

    shutdown button

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 function startWebsite() {
-  const startup = document.getElementById("startup");
+  const login = document.getElementById("login");
 
-  startup.classList.add("slide-up");
+  login.classList.add("slide-up");
 
   setTimeout(() => {
-    startup.style.display = "none";
+    login.style.display = "none";
   }, 3000);
 }
 
+function shutdown() {
+
+    login.classList.add("fade");
+    document.getElementById("shutdown").style.display = "flex";
+    setTimeout(() => {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("login").innerHTML = "";
+        document.getElementById("main-content").style.display = "none";
+        document.getElementById("main-content").innerHTML = "";
+    }, 1000);
+    setTimeout(() => {
+        window.open("https://rick.nerial.uk/video.mp4", "_self")
+    }, 5000);
+}
 
 (function () {
-  var clockElement = document.getElementById("clock");
-  var dateElement = document.getElementById("date");
+    var clockElement = document.getElementById("clock");
+    var dateElement = document.getElementById("date");
 
-  function updateClockandDate (clock, date) {
-    clock.innerHTML = new Date().toLocaleTimeString();
-    date.innerHTML = new Date().toLocaleDateString('en-US', {weekday: 'short', month: 'short', day: 'numeric'});
-  }
+    function updateClockandDate (clock, date) {
+      clock.innerHTML = new Date().toLocaleTimeString();
+      date.innerHTML = new Date().toLocaleDateString('en-US', {weekday: 'short', month: 'short', day: 'numeric'});
+    }
 
-  setInterval(function () {
-      updateClockandDate(clockElement, dateElement);
-  }, 1000);
+    setInterval(function () {
+        updateClockandDate(clockElement, dateElement);
+    }, 1000);
 
-  updateClockandDate(clockElement, dateElement);
+    updateClockandDate(clockElement, dateElement);
 }());
