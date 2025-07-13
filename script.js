6431307befea8d903559fbdfd3832d7d993e5c7a
noahniemeijer <noahniemeijer0603@gmail.com>
2025-07-13 16:58:28 +0200

    startup animation

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-function startWebsite() {
-  const login = document.getElementById("login");
+const text = document.getElementById('startup-text');
+    let dots = 0;
 
-  login.classList.add("slide-up");
+    setInterval(() => {
+        dots = (dots + 1) % 4;
+        text.textContent = 'Starting' + '.'.repeat(dots);
+    }, 500);
 
-  setTimeout(() => {
-    login.style.display = "none";
-  }, 3000);
-}
-
-function shutdown() {
-
-    login.classList.add("fade");
-    document.getElementById("shutdown").style.display = "flex";
     setTimeout(() => {
-        document.getElementById("login").style.display = "none";
-        document.getElementById("login").innerHTML = "";
-        document.getElementById("main-content").style.display = "none";
-        document.getElementById("main-content").innerHTML = "";
-    }, 1000);
+        startup.classList.add("fade");
+    }, 2500);
     setTimeout(() => {
-        window.open("https://rick.nerial.uk/video.mp4", "_self")
-    }, 5000);
-}
+        document.getElementById("startup").style.display = "none";
+        document.getElementById("startup").innerHTML = "";
+    }, 3500);
+    
 
 (function () {
     var clockElement = document.getElementById("clock");
@@ -38,3 +30,38 @@ function shutdown() {
 
     updateClockandDate(clockElement, dateElement);
 }());
+
+function startWebsite() {
+  const login = document.getElementById("login");
+
+  login.classList.add("slide-up");
+
+  setTimeout(() => {
+    login.style.display = "none";
+  }, 3000);
+}
+
+function shutdown() {
+    login.classList.add("fade");
+    document.getElementById("shutdown").style.display = "flex";
+    setTimeout(() => {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("login").innerHTML = "";
+        document.getElementById("main-content").style.display = "none";
+        document.getElementById("main-content").innerHTML = "";
+    }, 1000);
+
+    const text = document.getElementById('shutdown-text');
+    let dots = 0;
+
+    setInterval(() => {
+        dots = (dots + 1) % 4;
+        text.textContent = 'Shutting down' + '.'.repeat(dots);
+    }, 500);
+
+
+    setTimeout(() => {
+        window.open("https://rick.nerial.uk/video.mp4", "_self")
+    }, 5000);
+
+}
